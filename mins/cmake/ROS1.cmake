baba7b16cad008f0935eb8ff04106893e9d678b9
Woosik Lee <31552565+WoosikLee2510@users.noreply.github.com>
2026-07-09 22:55:22 -0700

    ros-free: build core + headless cli without ros, add no-ros CI

--- mins/cmake/ROS1.cmake
+++ mins/cmake/ROS1.cmake
@@ -1,23 +1,22 @@
 cmake_minimum_required(VERSION 3.5.1)
 
-find_package(catkin REQUIRED COMPONENTS roscpp rosbag tf std_msgs geometry_msgs sensor_msgs nav_msgs image_geometry visualization_msgs image_transport cv_bridge ov_core pcl_ros)
+# Find ROS1 (catkin). QUIET (not REQUIRED) so this same file can also configure a
+# ROS-free build of the core library when catkin is not present.
+find_package(catkin QUIET COMPONENTS roscpp rosbag tf std_msgs geometry_msgs sensor_msgs nav_msgs image_geometry visualization_msgs image_transport cv_bridge ov_core pcl_ros)
+option(ENABLE_ROS "Build the ROS integration and nodes when ROS is found" ON)
 
-add_definitions(-DROS_AVAILABLE=1)
-
-# Add catkin packages
-catkin_package(
-        CATKIN_DEPENDS roscpp rosbag tf std_msgs geometry_msgs sensor_msgs nav_msgs image_geometry visualization_msgs image_transport cv_bridge ov_core pcl_ros
-        INCLUDE_DIRS src/
-        LIBRARIES mins_lib
-)
+# When ROS is absent, ov_core (normally a catkin package) is pulled in as a subproject.
+# Do it here, before we populate LIBRARY_SOURCES: add_subdirectory shares this scope's
+# variables with the child, and ov_core also appends to LIBRARY_SOURCES.
+if (NOT (catkin_FOUND AND ENABLE_ROS))
+    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/../thirdparty/open_vins/ov_core ${CMAKE_BINARY_DIR}/ov_core)
+endif ()
 
 # Include our header files
 include_directories(
         src
         ${EIGEN3_INCLUDE_DIR}
-        ${PCL_INCLUDE_DIRS}
         ${Boost_INCLUDE_DIRS}
-        ${catkin_INCLUDE_DIRS}
         ${libpointmatcher_INCLUDE_DIRS}
 )
 
@@ -25,14 +24,10 @@ include_directories(
 list(APPEND thirdparty_libraries
         ${Boost_LIBRARIES}
         ${OpenCV_LIBRARIES}
-        ${catkin_LIBRARIES}
         ${libpointmatcher_LIBRARIES}
         )
 
-##################################################
-# Make the shared library
-##################################################
-
+# Core (ROS-free) library sources
 list(APPEND LIBRARY_SOURCES
         src/options/Options.cpp
         src/options/OptionsCamera.cpp
@@ -45,15 +40,11 @@ list(APPEND LIBRARY_SOURCES
         src/options/OptionsSystem.cpp
         src/options/OptionsVicon.cpp
         src/options/OptionsWheel.cpp
-        src/core/ROSPublisher.cpp
-        src/core/ROSSubscriber.cpp
-        src/core/ROSHelper.cpp
-        src/sim/Simulator.cpp
-        src/sim/ConstBsplineSE3.cpp
-        src/sim/SimVisualizer.cpp
         src/utils/PackagePath.cpp
         src/utils/Print_Logger.cpp
         src/utils/Jabdongsani.cpp
+        src/sim/Simulator.cpp
+        src/sim/ConstBsplineSE3.cpp
         src/state/State.cpp
         src/state/StateHelper.cpp
         src/state/Propagator.cpp
@@ -74,6 +65,46 @@ list(APPEND LIBRARY_SOURCES
         src/init/imu_wheel/IW_Initializer.cpp
         )
 
+if (catkin_FOUND AND ENABLE_ROS)
+    message(STATUS "MINS: building WITH ROS1")
+    add_definitions(-DROS_AVAILABLE=1)
+
+    # Add catkin packages
+    catkin_package(
+            CATKIN_DEPENDS roscpp rosbag tf std_msgs geometry_msgs sensor_msgs nav_msgs image_geometry visualization_msgs image_transport cv_bridge ov_core pcl_ros
+            INCLUDE_DIRS src/
+            LIBRARIES mins_lib
+    )
+    include_directories(${catkin_INCLUDE_DIRS} ${PCL_INCLUDE_DIRS})
+    list(APPEND thirdparty_libraries ${catkin_LIBRARIES})
+
+    # ROS integration lives at the boundary - compiled in only when we have ROS
+    list(APPEND LIBRARY_SOURCES
+            src/core/ROSPublisher.cpp
+            src/core/ROSSubscriber.cpp
+            src/core/ROSHelper.cpp
+            src/sim/SimVisualizer.cpp
+            )
+else ()
+    message(WARNING "MINS: building WITHOUT ROS - core library + headless CLI only")
+    add_definitions(-DROS_AVAILABLE=0)
+    include(GNUInstallDirs)
+    set(CATKIN_PACKAGE_LIB_DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    set(CATKIN_PACKAGE_BIN_DESTINATION "${CMAKE_INSTALL_BINDIR}")
+    set(CATKIN_GLOBAL_INCLUDE_DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/mins/")
+
+    # ov_core was pulled in above (before LIBRARY_SOURCES); just link + find PCL here.
+    # (In a ROS build PCL comes via catkin/pcl_ros; standalone we link it ourselves.)
+    find_package(PCL REQUIRED)
+    include_directories(${PCL_INCLUDE_DIRS})
+    link_directories(${PCL_LIBRARY_DIRS})
+    add_definitions(${PCL_DEFINITIONS})
+    list(APPEND thirdparty_libraries ov_core_lib ${PCL_LIBRARIES})
+endif ()
+
+##################################################
+# Make the shared library
+##################################################
 
 file(GLOB_RECURSE LIBRARY_HEADERS "src/*.h")
 add_library(mins_lib SHARED ${LIBRARY_SOURCES} ${LIBRARY_HEADERS})
@@ -93,26 +124,40 @@ install(DIRECTORY src/
 # Make binary files!
 ##################################################
 
-add_executable(simulation src/run_simulation.cpp)
-target_link_libraries(simulation mins_lib)
-install(TARGETS simulation
-        ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
-        )
+if (catkin_FOUND AND ENABLE_ROS)
 
-add_executable(bag src/run_bag.cpp)
-target_link_libraries(bag mins_lib)
-install(TARGETS bag
-        ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
-        )
+    add_executable(simulation src/run_simulation.cpp)
+    target_link_libraries(simulation mins_lib)
+    install(TARGETS simulation
+            ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+            )
 
-add_executable(subscribe src/run_subscribe.cpp)
-target_link_libraries(subscribe mins_lib)
-install(TARGETS subscribe
-        ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
-        RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
-        )
+    add_executable(bag src/run_bag.cpp)
+    target_link_libraries(bag mins_lib)
+    install(TARGETS bag
+            ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+            )
+
+    add_executable(subscribe src/run_subscribe.cpp)
+    target_link_libraries(subscribe mins_lib)
+    install(TARGETS subscribe
+            ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+            RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+            )
+
+else ()
+
+    # Headless CLI: runs the simulator through the estimator with no ROS, writes
+    # results to file. This is the ROS-free runnable target.
+    add_executable(mins_cli src/run_simulation_cli.cpp)
+    target_link_libraries(mins_cli mins_lib)
+    install(TARGETS mins_cli
+            RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+            )
+
+endif ()
